794e9658f810c857209db4495b92b0427ce4bda5
Ritesh Kumar <riteshkumarvk18@gmail.com>
2025-11-21 23:21:51 +0530

    Setup environment variables and project update

--- index.js
+++ index.js
@@ -1,91 +1,84 @@
 const express = require("express");
-const app= express();
-const path= require("path");
-const Chat= require("./Models/chat.js");
-const methodOverride= require("method-override");
+const app = express();
+const path = require("path");
+const Chat = require("./Models/chat.js");
+const methodOverride = require("method-override");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 app.use(methodOverride("_method"));
-app.set("views",path.join(__dirname,"views"));
-app.set("view engine","ejs");
-app.use(express.static(path.join(__dirname,"public")));
-app.use(express.urlencoded({extended:true}));
-
-const mongoose = require('mongoose');
-const { fromJSON } = require("postcss");
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
 
 main()
-.then(()=>{
-    console.log("connection successful");
-})
-.catch(err => console.log(err));
+  .then(() => {
+    console.log("MongoDB connection successful");
+  })
+  .catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/Whatsapp');
-
+  await mongoose.connect(process.env.MONGO_URI);
 }
 
-
-
-app.get("/",(req,res)=>{
-    res.send("root is working");
-});
-// index route
-app.get("/chats", async (req,res)=>{
-    let chats=  await Chat.find();
-   /* console.log(chats);*/
-    res.render("index.ejs",{chats});
+app.get("/", (req, res) => {
+  res.send("root is working");
 });
 
-// new route
-app.get("/chats/new",(req,res)=>{
+app.get("/chats", async (req, res) => {
+  let chats = await Chat.find();
+  res.render("index.ejs", { chats });
+});
+
+app.get("/chats/new", (req, res) => {
   res.render("new.ejs");
 });
 
-//create route
-app.post("/chats",(req,res)=>{
-    let{from,to,msg}= req.body;
-    let newChat= new Chat({
-        from:from,
-        to:to,
-        msg:msg,
-        created_at:new Date(),
-    });
+app.post("/chats", (req, res) => {
+  let { from, to, msg } = req.body;
 
-    newChat.save().then((res)=>{
-       console.log("chat was saved");
-    })
-    .catch((err)=>{
-        console.log(err);
-    });
+  let newChat = new Chat({
+    from,
+    to,
+    msg,
+    created_at: new Date(),
+  });
+
+  newChat
+    .save()
+    .then(() => console.log("chat was saved"))
+    .catch((err) => console.log(err));
 
   res.redirect("/chats");
 });
 
-//Edit route
-
-app.get("/chats/:id/edit",async (req,res)=>{
-    let{id}= req.params;
-    let chat= await Chat.findById(id);
-    res.render("edit.ejs",{chat});
+app.get("/chats/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  let chat = await Chat.findById(id);
+  res.render("edit.ejs", { chat });
 });
 
-// update route
-app.put("/chats/:id",async (req,res)=>{
-  let {id}= req.params;
-  let{msg: newMsg}= req.body;
-  let updatedChat= await Chat.findByIdAndUpdate(id,{msg:newMsg},{runValidators:true, new:true});
+app.put("/chats/:id", async (req, res) => {
+  let { id } = req.params;
+  let { msg: newMsg } = req.body;
+  let updatedChat = await Chat.findByIdAndUpdate(
+    id,
+    { msg: newMsg },
+    { runValidators: true, new: true }
+  );
   console.log(updatedChat);
   res.redirect("/chats");
 });
-// Destroy route
-app.delete("/chats/:id", async (req,res)=>{
-    let {id}= req.params;
-    let deletedChat =  await Chat.findByIdAndDelete(id);
-     console.log(deletedChat);
-     res.redirect("/chats");
-})
 
-app.listen(5000,()=>{
-    console.log("server is listening on port 5000");
+app.delete("/chats/:id", async (req, res) => {
+  let { id } = req.params;
+  let deletedChat = await Chat.findByIdAndDelete(id);
+  console.log(deletedChat);
+  res.redirect("/chats");
+});
 
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
